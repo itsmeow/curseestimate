f8023fd31615a47e52033a745229eefbc6c82fad
itsmeow <mods.itsmeow@gmail.com>
2020-03-24 18:40:20 -0400

    Migrate to new format

--- src/hooks/useEstimate.js
+++ src/hooks/useEstimate.js
@@ -2,44 +2,44 @@ import React, { useState, useEffect } from "react";
 import { toast } from "react-toastify";
 
 export default function useEstimate(slug, onError = () => {}) {
-  const [estimate, setEstimate] = useState();
-  const [loading, setLoading] = useState(false);
-  const [error, setError] = useState(false);
-  const [note, setNote] = useState();
+    const [estimate, setEstimate] = useState();
+    const [loading, setLoading] = useState(false);
+    const [error, setError] = useState(false);
+    const [note, setNote] = useState();
 
-  useEffect(() => {
-    const fetchEstimate = async () => {
-      setLoading(true);
-      setError(null);
+    useEffect(() => {
+        const fetchEstimate = async() => {
+            setLoading(true);
+            setError(null);
 
-      let res;
-      try {
-        res = await fetch(
-          `https://curse-point-estimator.herokuapp.com/estimate/${slug}`
-        );
-      } catch (error) {
-        setError(error.message);
-      }
+            let res;
+            try {
+                res = await fetch(
+                    `https://curseestimate.itsmeow.dev/estimate/${slug}`
+                );
+            } catch (error) {
+                setError(error.message);
+            }
 
-      if (res && res.ok) {
-        const json = await res.json();
-        if (json.status === "failed") {
-          onError(json);
-          setError(true);
-          setNote(json.note);
-        } else {
-          setError(false);
-          setEstimate(json);
+            if (res && res.ok) {
+                const json = await res.json();
+                if (json.status === "failed") {
+                    onError(json);
+                    setError(true);
+                    setNote(json.note);
+                } else {
+                    setError(false);
+                    setEstimate(json);
+                }
+            }
+
+            setLoading(false);
+        };
+
+        if (slug) {
+            fetchEstimate();
         }
-      }
+    }, [slug]);
 
-      setLoading(false);
-    };
-
-    if (slug) {
-      fetchEstimate();
-    }
-  }, [slug]);
-
-  return { loading, error, note, estimate };
+    return { loading, error, note, estimate };
 }
